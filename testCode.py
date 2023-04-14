d1b15fe13e7373b8eb2bea3cccb2203c0e93adf0
Kamil Spławiński <kamil.splawinski@nubisoft.io>
2023-04-15 00:20:46 +0200

    edit: first version (add basic filter and psd compile)

--- testCode.py
+++ testCode.py
@@ -1,23 +1,15 @@
-# testy
 import mne
 import numpy as np
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt
+# wczytaj dane
+raw = mne.io.read_raw_edf('PD129.edf',preload=True)
+events = mne.make_fixed_length_events(raw, duration=1.0)
 
-raw = mne.io.read_raw_edf('PD129.edf', preload=True)
-
-raw = raw.crop(tmax=120)
-
-raw.filter(l_freq=1, h_freq=40)
-
-
-sfreq = raw.info['sfreq']
-duration = raw.times[-1]
-n_samples = int(sfreq * duration)
-n_samples_per_second = int(sfreq)
-raw_resampled = raw.resample(sfreq=n_samples_per_second, n_samples=n_samples, npad="auto")
-
-freqs = np.logspace(*np.log10([1, 40]), num=60)
-tfr = mne.time_frequency.tfr_morlet(raw_resampled, freqs=freqs, n_cycles=freqs / 2,
-                                    return_itc=False, average=False, picks=None)
-
-tfr.plot(picks='eeg')
+# przetwórz dane
+raw.filter(1, 40, fir_design='firwin')
+epochs = mne.Epochs(raw, events, tmin=0, tmax=2, baseline=None)
+epo_spectrum = epochs.compute_psd(method='multitaper', fmin=5, fmax=30,
+                picks='eeg')
+psds, freqs = epo_spectrum.get_data(return_freqs=True)
